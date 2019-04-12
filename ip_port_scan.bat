42ae621c08f4d7f0177824492c66b397546037bb
remarklx <38095601+remarklx@users.noreply.github.com>
2019-04-12 17:47:07 +0800

    Create ip_port_scan.bat

---
+++ ip_port_scan.bat
@@ -0,0 +1,21 @@
+@echo off
+::set /p ip="ip field, prefix like "192.168.1." >>> "
+::set /p port="Port, like 20 >>> "
+set ip=10.251.101.1
+set port=20
+for /L %%i in (1,1,254) do (
+Ping.exe -n 1 -l 16 -w 100 %ip%%%i>>ipscan.txt
+if not errorlevel 1 (
+echo %ip%%%i yes
+D:\exe\tcping.exe %ip%%%i %port% > temp
+if not errorlevel 1 (
+echo Port%port% exist
+%ip%%%i >> valid_ip.txt
+)
+)
+)
+del temp
+del ipscan.txt
+echo valid ip with valid port %port% as followed:
+more valid_ip.txt
+pause
